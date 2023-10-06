f0631f38752112c702381635704793c402d64f66
tomek <tbryndza86@gmail.com>
2023-10-06 11:33:58 +0200

    123

---
+++ session/session2.php
@@ -0,0 +1,10 @@
+<?php
+  session_start();
+  echo 'Welcome to page #2<br>';
+
+  echo "Ulubiony kolor: ".$_SESSION['favcolor'];
+  echo "Ulubione zwierze: ".$_SESSION['animal'].'<br>';
+  echo date('Y m d h:i:s',$_SESSION['time']).'<br>';
+
+  echo $_SESSION['123'];
+    ?>
