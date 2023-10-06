1c912b8775143b7d6050fd4e6d6f4a065887bd9c
tomek <tbryndza86@gmail.com>
2023-10-06 12:17:37 +0200

    123

--- usuwanie cookie/usuwaniecookie.php
+++ usuwanie cookie/usuwaniecookie.php
@@ -1,10 +1,6 @@
 <?php
-$cookie_name = "user";
-setcookie($cookie_name, "", time() - 3600 );
-if(!isset($_COOKIE[$cookie_name])) {
-    echo "Cookie named '" . $cookie_name . "' is not set!";
-} else {
-    echo "Cookie '" . $cookie_name . "' is set! <br>";
-    echo "Value is: " . $_COOKIE[$cookie_name];
-}
+$cookie_name = "promocje";
+$cookie_value = "wycieczka";
+setcookie($cookie_name, $cookie_value);
+            time() + (48*60*60); 
 ?>
